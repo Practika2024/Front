3961694f55ae924e8831a5e3620d1ed708211448
Vetal05 <118655581+Vetal05@users.noreply.github.com>
2025-02-05 10:23:15 +0200

    Add Sidebar component

--- TrackTara/src/App.jsx
+++ TrackTara/src/App.jsx
@@ -1,13 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import AppRouter from "./router/AppRouter";
 
 function App() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
+
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </Router>
+        <div className={`flex h-screen ${isSidebarOpen ? "sidebar-open" : ""}`}>
+            {/* Сайдбар */}
+            <Sidebar
+                isOpen={isSidebarOpen}
+                toggleSidebar={toggleSidebar} // Функція для зміни стану
+            />
+
+            {/* Основний контент */}
+            <div
+                className={`flex-1 p-4 transition-all duration-300 ${
+                    isSidebarOpen ? "ml-64" : "ml-0"
+                }`} // Зсув основного контенту при відкритому сайдбарі
+            >
+                <AppRouter />
+            </div>
+        </div>
     );
 }
 
--- TrackTara/src/main.jsx
+++ TrackTara/src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AppRouter />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
