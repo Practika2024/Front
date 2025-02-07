1789250de3205d06804a85fc65f99987f351afbc
Vetal05 <118655581+Vetal05@users.noreply.github.com>
2025-02-07 15:39:49 +0200

    Download project TEMPLATE

--- TrackTara/src/App.jsx
+++ TrackTara/src/App.jsx
@@ -1,32 +1,13 @@
-import React, { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import AppRouter from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import BasicRoute from "./routes/BasicRoute";
 
 function App() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-
-    return (
-        <div className={`flex h-screen ${isSidebarOpen ? "sidebar-open" : ""}`}>
-            {/* Сайдбар */}
-            <Sidebar
-                isOpen={isSidebarOpen}
-                toggleSidebar={toggleSidebar} // Функція для зміни стану
-            />
-
-            {/* Основний контент */}
-            <div
-                className={`flex-1 p-4 transition-all duration-300 ${
-                    isSidebarOpen ? "ml-64" : "ml-0"
-                }`} // Зсув основного контенту при відкритому сайдбарі
-            >
-                <AppRouter />
-            </div>
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <BasicRoute />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- TrackTara/src/main.jsx
+++ TrackTara/src/main.jsx
@@ -1,13 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { AuthByToken } from "./store/state/actions/userActions";
+import { ToastContainer } from "react-toastify";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+if (localStorage.accessToken && localStorage.refreshToken) {
+  AuthByToken({
+    accessToken: localStorage.accessToken,
+    refreshToken: localStorage.refreshToken,
+  })(store.dispatch);
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      hideProgressBar
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable={false}
+      pauseOnHover={false}
+      theme="light"
+    />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
