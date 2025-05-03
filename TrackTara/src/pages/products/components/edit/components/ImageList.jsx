3f4b102ebde6eb8e4cdf60297f3d2b178514e93f
andromaan <146390373+andromaan@users.noreply.github.com>
2025-05-03 17:54:11 +0300

    added not found images for products

--- TrackTara/src/pages/products/components/edit/components/ImageList.jsx
+++ TrackTara/src/pages/products/components/edit/components/ImageList.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import NoImageProduct from "../../../../../assets/images/noImageProduct.png";
 import ImageItem from "./ImageItem";
 
 const ImageList = ({ onRemove, imagesToDelete }) => {
+
   const images = useSelector((state) => state.product.product.images);
 
   const visibleImages = images.filter((image) => !imagesToDelete.includes(image.id));
@@ -11,7 +11,7 @@ const ImageList = ({ onRemove, imagesToDelete }) => {
   return (
     <div>
       {visibleImages.length === 0 ? (
-        <img height="200" alt="Product Image" src={NoImageProduct} />
+        <img height="200" alt="Product Image" src="/images/noImageProduct.png" />
       ) : (
         <div className="d-flex gap-3 flex-wrap">
           {visibleImages.map((image) => (
